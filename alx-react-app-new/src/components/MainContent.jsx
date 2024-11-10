0bf54b7ee6398cab1b501bd2b9521ad853c97e20
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-11-10 10:29:22 -0800

    new exercise

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,8 @@
 const MainContent = () => {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
-      <h2 style={{ color: 'darkgreen' }}>Explore the World</h2>
+      <h1>I love to visit New York, Paris, and Tokyo.<h1/>
+      <h2 style={{ color: 'darkgreen' }}>The world</h2>
       <p style={{ lineHeight: '1.5' }}>Discover various cities and cultures.</p>
     </main>
   );
