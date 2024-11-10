1a60eab8a5c1a9f068c1cfaf57f12d777bebb568
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-11-10 10:01:15 -0800

    new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+const MainContent = () => {
+    return (
+      <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
+        <h2 style={{ color: 'darkgreen' }}>Explore the World</h2>
+        <p style={{ lineHeight: '1.5' }}>Discover various cities and cultures.</p>
+      </main>
+    );
+  };
+  
+  export default MainContent;
