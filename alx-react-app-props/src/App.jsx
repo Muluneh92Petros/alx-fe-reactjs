9f9df8b80af1da4920327ba1ef559fdc712e0feb
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-11-10 11:35:14 -0800

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -10,7 +10,20 @@ import UserProfile from './UserProfile'
 
 import React from 'react';
 import Counter from './components/Counter';
+import ProfilePage from './ProfilePage';
+import UserContext, { UserProvider } from './UserContext';
 
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserProvider value={userData}>
+      <ProfilePage />
+    </UserProvider>
+  );
+}
+
+export default App;
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
