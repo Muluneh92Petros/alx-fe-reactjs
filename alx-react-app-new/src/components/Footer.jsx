0d87d1849d0333d25cb9a43d286fcb815524e5f9
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-11-10 10:01:55 -0800

    new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer style={{ backgroundColor: 'darkslategray', color: 'white', textAlign: 'center', padding: '10px' }}>
+        <p>© 2024 My Favorite Cities</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
