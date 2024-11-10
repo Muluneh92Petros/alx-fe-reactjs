50fd3322930c29f802ae4dcfca0bb17f3288f4f7
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-11-10 12:17:42 -0800

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+const Navbar: React.FC = () => {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white' }}>
-      <Link to="/" style={{ color: 'white', margin: '10px' }}>Home</Link>
-      <Link to="/about" style={{ color: 'white', margin: '10px' }}>About</Link>
-      <Link to="/services" style={{ color: 'white', margin: '10px' }}>Services</Link>
-      <Link to="/contact" style={{ color: 'white', margin: '10px' }}>Contact</Link>
+    <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white', display: 'flex', justifyContent: 'space-around' }}>
+      <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
     </nav>
   );
-}
+};
 
 export default Navbar;
