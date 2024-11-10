fa40bd44a3f4f0fb8b1313c9836de22c60f75fbb
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-11-10 10:56:07 -0800

    new

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,20 @@ import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
 import UserProfile from './UserProfile'
-import Counter from './components/Counter'
+
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Counter Application</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
 function App() {
   const [count, setCount] = useState(0)
 
