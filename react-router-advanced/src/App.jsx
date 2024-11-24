5afd0aabf6a4de8006560a1a2be446b33ee7c486
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-11-24 13:00:05 -0800

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="BrowserRouter">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
