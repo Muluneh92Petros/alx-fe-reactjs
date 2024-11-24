ba8f3241911ae16569f735f588be16783b1b8eaf
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-11-24 13:02:02 -0800

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children, isAuthenticated }) => {
-  return isAuthenticated ? children : <Navigate to="/" />;
+  return isAuthenticated ? children : <Navigate to="useAuth/" />;
 };
 
 export default ProtectedRoute;
