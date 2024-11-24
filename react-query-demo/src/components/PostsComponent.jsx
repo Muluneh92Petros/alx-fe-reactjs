7e66e6f55f71c74f2f9abb1a745d4896756406bb
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-11-24 12:43:21 -0800

    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -13,7 +13,7 @@ const PostsComponent = () => {
   const { data, isLoading, error, refetch } = useQuery('posts', fetchPosts);
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  if (isError) return <div>Error: {error.message}</div>;
 
   return (
     <div>
