df8941010d87f8ebde16025a3e5ca01b97d20b47
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-12-04 07:45:37 -0800

    recipe

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import data from '../data.json';
 
-const RecipeDetail = () => {
+const RecipeDetail = (shadow) => {
   const { id } = useParams();
   const [recipe, setRecipe] = useState(null);
 
