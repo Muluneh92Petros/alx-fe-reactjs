d423a027b6af19ced33b6b05f40e95d538280131
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-12-04 07:50:29 -0800

    recipe

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import data from '../data.json';
 
-const HomePage = (md) => {
+const HomePage = (md,Link,react-router-dom) => {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
