ac6c3f03ee65a07a5bea030893d37d8e5aeaa52f
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-12-06 11:22:57 -0800

    recipe

--- recipe-sharing-platform/src/App.js
+++ recipe-sharing-platform/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import AddRecipeForm from './component/AddRecipeForm';
+import AddRecipeForm from './components/AddRecipeForm';
 
 const AddRecipeForm = () => {
     const [title, setTitle] = useState('');
