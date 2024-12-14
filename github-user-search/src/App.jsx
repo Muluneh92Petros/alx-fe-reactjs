cba0fc0da6e892ce7d108c15f29ef2d5547a7a6d
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-12-14 12:57:10 -0800

    user

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Search from './components/Search';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
       <div>
       <h1>GitHub User Search</h1>
+      <Search />
       {/* Add components here */}
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -35,4 +37,6 @@ function App() {
 }
 
 export default App
+
+
 
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,1 +1,7 @@
-const apiKey = process.env.REACT_APP_GITHUB_API_KEY;
+// src/services/githubService.js
+import axios from 'axios';
+
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+};
